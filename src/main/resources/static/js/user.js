7fa72d0849f143d6b1a9076c5255a7af66a4c3cb
jhy <jeonghwayun0119@naver.com>
2022-03-11 13:50:06 +0900

    220311

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,36 @@
+let index = {
+	init: function(){
+		$("#btn-save").on("click", () => { // function(){}을 사용하지 않고, ()=>{}을 사용하는 이유는 this를 바인딩하기 위해서
+			this.save();
+		});
+	},
+	
+	save: function() {
+		//alert('user의 save 함수 호출됨');
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		};
+		 
+		
+		// ajax 호출 시 default가 비동기 흐름
+		// ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+		// ajax 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환
+		$.ajax({
+			type: "POST",
+			url: "/auth/joinProc",
+			data: JSON.stringify(data), // http body 데이터
+			contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
+			dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴 게 json이라면) => javascript 오브젝트로 변경
+		}).done(function(resp){           
+			alert("회원가입이 완료되었습니다.");
+			//console.log(resp);
+			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청 
+	},
+} 
+
+index.init(); 
